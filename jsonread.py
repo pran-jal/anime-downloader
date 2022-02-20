ddfa0c11fe4ffabb00df7080aba67994a04a9bf2
Pranjal <72604669+pran-jal@users.noreply.github.com>
2022-02-20 14:13:06 +0530

    json data stored

--- jsonread.py
+++ jsonread.py
@@ -1,7 +1,7 @@
-
 import json
 
 def url() :
-    f = open("data.json")
-    a = json.load(f)
+    f = open("data.txt", 'r')
+    string = f.read()
+    a = json.loads(string)
     return a["@graph"][0]["video"]["embedUrl"]
