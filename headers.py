b45058b55a6e45011e8f63ecd8fbe50c4ff2a781
Pranjal <72604669+pran-jal@users.noreply.github.com>
2022-03-02 15:30:29 +0530

    Update and rename old/headers.py to headers.py

--- headers.py
+++ headers.py
@@ -1,7 +1,6 @@
 headers = {
     'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
     'Host': 'vidstream.pro',
-    #'Host': 'mcloud.to',
     'Connection': 'keep-alive',
     'Referer': 'https://animeheaven.pro/',
 }
