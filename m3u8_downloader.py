9f1f1fa6e7a306d644a212ef54ef26dcb4705cc6
Pranjal <72604669+pran-jal@users.noreply.github.com>
2022-03-02 15:31:38 +0530

    Update and rename old/m3u8_downloader.py to m3u8_downloader.py

--- m3u8_downloader.py
+++ m3u8_downloader.py
@@ -2,4 +2,4 @@ import subprocess
 
 def m3u8_downloader(url, name) :
     s = 'ffmpeg -i "%s" -c copy %s.mp4 -y' %(url, name)
-    subprocess.run(["powershell", "-command", f'Start-Process PowerShell -ArgumentList \'-NoExit {s}\' '] )
+    subprocess.run(["powershell", "-command", f"Start-Process PowerShell -ArgumentList \'-NoExit {s}\' "] )
