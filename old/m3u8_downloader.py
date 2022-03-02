ddfe979e008b19839e506ade9c373c589be2040f
Pranjal <72604669+pran-jal@users.noreply.github.com>
2022-03-02 15:38:09 +0530

    Delete old directory

--- old/m3u8_downloader.py
+++
@@ -1,5 +0,0 @@
-import subprocess
-
-def m3u8_downloader(url, name) :
-    s = 'ffmpeg -i "%s" -c copy %s.mp4 -y' %(url, name)
-    subprocess.run(["powershell", "-command", f'Start-Process PowerShell -ArgumentList \'-NoExit {s}\' '] )
