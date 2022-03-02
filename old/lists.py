14a7ebc90cef8c59e929b4342a04992052a379ff
Pranjal <72604669+pran-jal@users.noreply.github.com>
2022-03-02 15:15:21 +0530

    Delete old directory

--- old/lists.py
+++
@@ -1,8 +0,0 @@
-import headers
-import requests as r
-
-
-def lists(source) :
-    del headers.headers['Referer']
-    headers.headers['Host'] = source.split('/')[2]
-    return r.get(source, headers=headers.headers)
--- old/sources.py
+++
@@ -1,7 +0,0 @@
-import requests as r
-import headers
-
-def sources(url, embedurl) :
-    headers.headers['Referer'] = embedurl
-    lists = r.get(url, headers=headers.headers)
-    return lists
