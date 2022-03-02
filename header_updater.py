a034037cc02cf8f0d658c99fcd0c9e5296921902
Pranjal <72604669+pran-jal@users.noreply.github.com>
2022-03-02 15:09:37 +0530

    Delete header_updater.py

--- header_updater.py
+++
@@ -1,8 +0,0 @@
-from email import header
-import headers
-
-def updater(host):
-    if host == 'vidstream':
-        headers.headers['Host'] = 'vidstream.pro'
-    elif host == 'mcloud':
-        headers.headers['Host'] = 'mcloud.to'
