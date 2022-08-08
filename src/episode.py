803e7d29df91ec0d00a2a61702f5afd94abc747e
Pranjal <72604669+pran-jal@users.noreply.github.com>
2022-08-08 23:38:55 +0530

    episode get json data

--- src/episode.py
+++ src/episode.py
@@ -1,13 +1,10 @@
 import sys
 sys.path.insert(1, './src')
 import namevarifier
-import progress_bar
 import resolution
 import json_server
 
-def download_episode(url):
-    lists = json_server.get_json([url])
-    # print(lists)
+def download_episode(url, lists, dir_name=None):
     episode = lists.json()['data']['media']['sources'][1]['file']
     res = resolution.resolutions(episode)
     episode = episode[::-1]
@@ -16,9 +13,12 @@ def download_episode(url):
             break
     episode = episode[i::][::-1]+res[0]
     name = namevarifier.namevarifier(url.split('/watch/')[1][:-1])
-    bar = progress_bar.ProgressBar(episode, name, name)
-    bar.downloader()
-    # return progress_bar.downloader(episode, name, name)
+    if dir_name is None:
+        dir_name=name
+    return downloader.downloader(episode, name, dir_name, True)
+    # bar = progress_bar.ProgressBar(episode, name, dir_name)
+    # bar.downloader()
+    # # return progress_bar.downloader(episode, name, name)
 
 
 if __name__ == '__main__' :
