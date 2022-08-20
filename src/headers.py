c1031ec21719c8f5ec7a1ed2a1d0449f0f54f17b
Pranjal <72604669+pran-jal@users.noreply.github.com>
2022-08-20 18:43:09 +0530

    moved to obsolete

--- src/headers.py
+++
@@ -1,7 +0,0 @@
-headers = {
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8',
-    'Connection': 'keep-alive',
-    'origin': 'https://animeheaven.pro',
-    'Referer': 'https://animeheaven.pro/',
-    'Host': 'vizcloud.cloud',
-}
