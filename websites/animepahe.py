0b95898481f43adfa4002895654b50e94e1d4836
Pranjal <72604669+pran-jal@users.noreply.github.com>
2022-09-13 14:37:07 +0530

    Complete pahe

---
+++ websites/animepahe.py
@@ -0,0 +1,88 @@
+import re
+import sys
+import requests as r
+sys.path.insert(1, './')
+
+from src.downloader import download
+from src.pahereader import reader
+from src.titlecheck import validate
+
+headers = {
+    'referer': 'https://animepahe.org/',
+    'origin': 'https://kwik.cx',
+    'referer': 'https://kwik.cx/'
+}
+
+user_agent = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36'
+}
+
+
+def all_epi_url(url):
+    episode_page = r.get(url).text
+    read = reader()
+    read.feed(episode_page)
+    episode_list = read.episode_list
+    session_ids = {}
+    total_episodes = len(episode_list)
+    for i in range(total_episodes):
+        session_ids[i+1] = episode_list[i].split('/').pop()
+    return session_ids
+
+def all_epi_url_from_api(url):         # this can bypass the use of pahereader, may be faster
+    url = url.split('/')
+    
+    if len(url)<5:
+        raise ValueError("season_id Not Found. Invalid URL")
+    
+    season_id = url[4]
+    api_url = f"{url[0]}" + "//" + f"{url[2]}/api?m=release&id={season_id.split('?')[0]}&sort=episode_asc&"
+    session_ids = {}
+    page="page=1"
+    
+    while page:
+        page = page.split('?').pop()
+        session_ids_json = r.get(api_url + page, headers=user_agent).json()
+        page = session_ids_json["next_page_url"]
+        data = session_ids_json["data"]
+        for item in data:
+            session_ids[item["episode"]] = item["session"]
+
+    return session_ids
+
+def embeds_seperate(json_data):
+    eng = {}
+    jpn = {}
+
+    for item in json_data:
+        for key, value in item.items():
+            eval(value["audio"])[key] = value["kwik"]
+        
+    return jpn
+
+def main(url = None):
+    if url == None:
+        url = input("Url: ")
+    sessions = all_epi_url_from_api(url)
+
+    episodes = []
+    for id in sessions:
+        episode = {}
+        episode["url"] = f'https://animepahe.com/api?m=links&id={sessions[id]}&p=kwik'
+        embed_urls = embeds_seperate(r.get(episode["url"], headers=user_agent).json()['data'])# .pop() gives the last value which is the data of the best resolution.
+        embed_page = r.get(embed_urls["1080"], headers=headers).text
+        episode["title"] = validate(re.findall(re.compile("<title>[\w-]+.mp4</title>"), embed_page)[0].split('>')[1].split(".mp4")[0])
+        session_vala_chunk = re.findall(re.compile("<script>.*[\s\S]*?(?=</script>)"), embed_page)[0]
+        session = '/'.join(session_vala_chunk.split('eval(function(').pop().split('|m3u8|uwu|')[1].split("'.split('|')")[0].split('|')[2::-1])
+        episode["down_link"] = "https://eu-191.files.nextcdn.org/" + session + "/uwu.m3u8"
+        episodes.append(episode)
+
+    dir_name = episodes[0]["title"].split("_-_")[0]
+    referer = 'https://kwik.cx/'
+
+
+    # this also need to send referer kwik to ffmpeg
+    download(episodes, dir_name, referer)
+
+if __name__ == "__main__":
+    main("https://animepahe.com/anime/17b49872-8b62-9721-7884-813747531544")
